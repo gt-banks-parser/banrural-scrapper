76a3f9061594dcd6b28ba098f7ec6bc530b81e74
Carlos Simon <git@csimon.dev>
2021-10-05 13:25:22 +0000

    Implement get_account method. Implement alternative_transaction_id

--- setup.py
+++ setup.py
@@ -22,10 +22,8 @@ setup(
         "Intended Audience :: Financial and Insurance Industry",
         "Intended Audience :: Information Technology",
         "Topic :: Office/Business :: Financial",
-        "Topic :: Utilities"
-    ], 
+        "Topic :: Utilities",
+    ],
     python_requires=">=3.6",
-    install_requires=[
-        "bank_base_gt>=0.2"
-    ]
+    install_requires=["bank_base_gt>=0.3"],
 )
