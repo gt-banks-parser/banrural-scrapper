84dd91e650d5d4ea03906a311589b140dd031eca
Carlos Simon <dev@csimon.dev>
2021-10-06 12:03:28 -0600

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="banrural_bank_parser_gt",
-    version="0.2.1",
+    version="0.2.4",
     author="Carlos Simon",
     author_email="dev@csimon.dev",
     description="Banrural Parser",
